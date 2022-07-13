57c1a0d64c7aab49d32cf0db2f6714725e9d0a26
zzerowin <alaykr123@gmail.com>
2022-07-13 15:03:53 +0900

    [추가] 컨트롤러로 분리

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,5 @@
 package com.ll.exam;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -14,35 +12,26 @@ public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
 
-        int wiseSayingsLastId = 0;
-        List<WiseSaying> wiseSayings = new ArrayList<>();
+        WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
         outer:
         while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
+            Rq rq = new Rq(cmd);
 
-            switch (cmd) {
+            switch (rq.getPath()) {
                 case "등록":
-                    int id = ++wiseSayingsLastId;
-                    System.out.print("명언 : ");
-                    String content = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-
-                    wiseSayings.add(new WiseSaying(id, content, author));
-
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    wiseSayingController.write(rq);
                     break;
                 case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-                        WiseSaying wiseSaying = wiseSayings.get(i);
-
-                        System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.author, wiseSaying.content);
-                    }
+                    wiseSayingController.list(rq);
+                    break;
+                case "삭제":
+                    wiseSayingController.remove(rq);
+                    break;
+                case "수정":
+                    wiseSayingController.modify(rq);
                     break;
                 case "종료":
                     break outer;
