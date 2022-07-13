32917b312bb039d70cefdbdb6c32e636feb081a3
zzerowin <alaykr123@gmail.com>
2022-07-13 13:39:32 +0900

    [추가]종료 기능 테스트

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -9,11 +9,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+    @Test
+    public void 프로그램_시작시_타이틀_출력_그리고_종료() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertTrue(rs.contains("== 명언 SSG =="));
+        assertTrue(rs.contains("명령)"));
+    }
 
     @Test
     public void 테스트() {
         assertTrue(1 == 1);
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
     }
 
     @Test
@@ -25,12 +46,12 @@ public class AppTest {
     }
 
     @Test
-    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-        System.out.print("안녕");
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+    public void 문자열을_파일에_저장() {
+        Util.file.mkdir("test_data");
+        Util.file.saveToFile("test_data/1.txt", "안녕");
 
-        assertEquals("안녕", rs);
+        String body = Util.file.readFromFile("test_data/1.txt");
+
+        assertEquals("안녕", body);
     }
 }
